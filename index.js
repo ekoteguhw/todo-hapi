353cdc92028d40719d4207532b944f099616b3e0
Eko Teguh Widodo <id.ekoteguhw@gmail.com>
2018-07-09 10:34:41 +0700

    Implement Swagger

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ require('dotenv').config()
 const Todo = require('./models/Todo')
 const { graphqlHapi, graphiqlHapi } = require('apollo-server-hapi')
 const schema = require('./graphql/schema')
+const Inert = require('inert')
+const Vision = require('vision')
+const HapiSwagger = require('hapi-swagger')
+const Pack = require('./package')
+const Joi = require('joi')
 
 const server = hapi.server({
   port: 4000,
@@ -17,6 +22,20 @@ monggose.connection.once('open', () => {
 })
 
 const init = async () => {
+
+  await server.register([
+    Inert,
+    Vision,
+    {
+      plugin: HapiSwagger,
+      options: {
+        info: {
+          title: 'Todo API documentation',
+          version: Pack.version
+        }
+      }
+    }
+  ])
 
   await server.register({
     plugin: graphiqlHapi,
@@ -53,24 +72,67 @@ const init = async () => {
   }, {
     method: 'GET',
     path: '/api/v1/todos',
-    handler: (req, res) => {
-      return Todo.find()
+    config: {
+      description: 'Get all the todos',
+      tags: ['api', 'v1', 'todos', 'get'],
+      handler: (req, res) => {
+        return Todo.find()
+      }
+    }
+  }, {
+    method: 'GET',
+    path: '/api/v1/todos/{id}',
+    config: {
+      description: 'Get a specific todo by ID',
+      tags: ['api', 'v1', 'todo', 'get'],
+      validate: {
+        params: {
+          id: Joi.string().required()
+        }
+      },
+      handler: (req, res) => {
+        const id = req.params.id
+        return Todo.findById(id)
+      }
     }
   }, {
     method: 'POST',
     path: '/api/v1/todos',
-    handler: (req, res) => {
-      const { title, description, isCompleted } = req.payload
-      const todo = new Todo({
-        title, description, isCompleted
-      })
+    config: {
+      description: 'Post a todo',
+      tags: ['api', 'v1', 'todo', 'post'],
+      plugins: {
+        'hapi-swagger': {
+          payloadType: 'form'
+        }
+      },
+      validate: {
+        payload: Joi.object({
+          title: Joi.string().required(),
+          description: Joi.string().required(),
+          isCompleted: Joi.boolean().required()
+        })
+      },
+      handler: (req, res) => {
+        const { title, description, isCompleted } = req.payload
+        const todo = new Todo({
+          title, description, isCompleted
+        })
 
-      return todo.save()
+        return todo.save()
+      }
     }
   }])
 
   await server.start()
   console.log(`Server running at: ${server.info.uri}`)
 }
+
+process.on('unhandledRejection', (err) => {
+  if (err) {
+    console.log(err)
+    process.exit(1)
+  }
+})
 
 init()
